eeaefabba89abf2864313c1fb67be55275e2a080
FNieto11 <facundo.nieto@live.com.ar>
2023-09-05 20:31:12 -0300

    Comenzando

--- script/script.js
+++ script/script.js
@@ -11,21 +11,27 @@ function comenzar() {
     palabraOculta = '';
     letraProbada = '';
     letraIncorrecta = '';
+    //dejar todo en cero para poder empezar el juego
 }
 
 function verificarLetraProbada(letra) {
-    
+    //Comparar letra ingresada con letras en palabra oculta
 }
 
 
 if(fallos == 0){
-    
+    //Mostrar imagen 0
 }else if(fallos == 1){
-    
+    //Mostrar imagen 1
 }else if(fallos == 2){
-    
+    //Mostrar imagen 2
 }else if(fallos == 3){
-    
+    //Mostrar imagen 3
 }else if(fallos == 4){
-    
-}  
+    //Mostrar imagen 4
+}
+
+function ganar() {
+    //Mostrar imagen ganador
+    alert("Felicidades... ¡¡Acertaste!!")
+}
